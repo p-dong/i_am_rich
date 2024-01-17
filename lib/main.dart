fed69a682ee1e80d5f777b3723ef6f2839189cf8
pradipDong <tamangpradip20@gmail.com>
2024-01-17 10:43:57 +0545

    learned basics about the dart language

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar(
+            title: Text(
+              'I am Rich',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            backgroundColor: Colors.blueGrey[900],
+          ),
+          body: Center(
+            child: Image(
+              image: AssetImage('Images/diamond.png'),
+            ),
+          ),
+        ),
+      ),
+    );
